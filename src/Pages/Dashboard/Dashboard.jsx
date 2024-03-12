9936e804d20c60c8023adf10272b11d97743b4cf
dilsara <patabadigerajitha@gmail.com>
2024-03-12 12:46:45 +0530

    all resolved dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -4,6 +4,8 @@ import Navbar from "../../Components/Structure/Navbar";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import AppsIcon from '@mui/icons-material/Apps';
+import Card from "../../Components/DashBoard/Card"
+import BarChart from "../../Charts/BarChart";
 export default function Dashboard() {
   return (
   <Box  sx={{bgcolor: "#e0f2f1",minHeight:"100vh"}}>
@@ -13,12 +15,13 @@ export default function Dashboard() {
         <Sidenav />
         <Box component="main" sx={{ flexGrow: 1, p: 3,m:3,bgcolor:"white",boxShadow:1  }}>
         <Box sx={{mx:3}}>
-        <Typography sx={{fontSize:30,fontWeight: 'bold'}}>
+        {/* <Typography sx={{fontSize:30,fontWeight: 'bold'}}>
         <AppsIcon sx={{mr:3}}/>
           Dashboard
-        </Typography>
+        </Typography> */}
+        <Card />
         </Box>
-          contend
+          {/* contend */}
         </Box>
       </Box>
       </Box>
