7045d8321570f843d3f21627234342b06c0228f0
malinda <malisur490@gmail.com>
2024-06-16 23:43:39 +0530

    frontEnd create table complte

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -10,44 +10,52 @@ export const CheckEmpty = (data) => {
 
 export const senderValidation = (sname,saddress,stele) => {
 if(CheckEmpty(sname)){ 
-    return ({textField:"Name", Error:"Name is Compulsary"})
+    return ({textField:"Name", Error:"Name is required"})
   }else if(CheckEmpty(stele)){
-    return ({textField:"Telephone", Error:"Telephone number is Compulsary"})
+    return ({textField:"Telephone", Error:"Telephone number is required"})
   }else if(CheckEmpty(saddress)){
-    return ({textField:"Address", Error:"Address is Compulsary"}) 
+    return ({textField:"Address", Error:"HomeTown is required"}) 
   }else if(!stele.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)){
     return ({textField:"Telephone", Error:"Invalid Telephone number"})
   }else
   return 0
 };
 
-export const RecieverValidation = (rname,rdistrict,rhometown,rtele) => {
+export const RecieverValidation = (rname,rdistrict,rhometown,rtele,rprovince) => {
 if(CheckEmpty(rname)){ 
-    return ({textField:"Name", Error:"Name Error"})
-  }else if(CheckEmpty(rdistrict)){
-    return ({textField:"District", Error:"District Error"})
-  }else if(CheckEmpty(rhometown)){
-    return ({textField:"HomeTown", Error:"Hometown Error"})
+    return ({textField:"Name", Error:"Name is required"})
   }else if(CheckEmpty(rtele)){
-    return ({textField:"Telephone", Error:"Telephone Error"})
+    return ({textField:"Telephone", Error:"Telephone is required"})
+  }else if(CheckEmpty(rprovince)){
+    return ({textField:"rprovince", Error:"Province is Compulsory"})
+  }else if(CheckEmpty(rdistrict)){
+    return ({textField:"District", Error:"District is required"})
+  }else if(CheckEmpty(rhometown)){
+    return ({textField:"HomeTown", Error:"Hometown is required"})
   }else if(!rtele.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)){
     return ({textField:"Telephone", Error:"Invalid Telephone number"})
   }else
   return 0
 };
-export const PickupValidation = (pvehical,paddress,phometown,pPayment,ptele,pdistric) => {
-if(CheckEmpty(pvehical)){ 
-    return ({textField:"vehical", Error:"Vehical Error"})
-  }else if(CheckEmpty(paddress)){
-    return ({textField:"Address", Error:"Address Error"})
-  }else if(CheckEmpty(phometown)){
-    return ({textField:"HomeTown", Error:"Hometown Error"})
-  }else if(CheckEmpty(pPayment)){
-    return ({textField:"payment", Error:"Payment Error"})
+export const PickupValidation = (phometown,pPayment,ptele,pdistric,pbranch,plevel,pcost,pvehical) => {
+ if(CheckEmpty(phometown)){
+  return ({textField:"HomeTown", Error:"Hometown is required"})
+}else if(CheckEmpty(pbranch)){ 
+  return ({textField:"branch", Error:"Branch is required"})
+}else if(CheckEmpty(pdistric)){
+  return ({textField:"Distric", Error:"Distric is required"})
+}else if(CheckEmpty(ptele)){
+  return ({textField:"Telephone", Error:"Telephone is required"})
+}else if(CheckEmpty(pPayment)){
+  return ({textField:"payment", Error:"Payment is required"})
+  }else if(CheckEmpty(pvehical)){ 
+    return ({textField:"vehical", Error:"Vehical is required"})
+  }else if(CheckEmpty(plevel)){ 
+    return ({textField:"level", Error:"Priority Level is required"})
+  }else if(CheckEmpty(pcost)){
+    return ({textField:"Address", Error:"Distance Cost is  is required"})
   }else if(CheckEmpty(ptele)){
     return ({textField:"Telephone", Error:"Telephone Error"})
-  }else if(CheckEmpty(pdistric)){
-    return ({textField:"Distric", Error:"Distric Error"})
   }else if(!ptele.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)){
     return ({textField:"Telephone", Error:"Invalid Telephone number"})
   }else
