b25518e161f3872bb82603b1a56395babf7043a8
malinda <malisur490@gmail.com>
2024-07-10 02:21:15 +0530

    forgot page

--- src/Pages/SignIn/SigninStyles.js
+++ src/Pages/SignIn/SigninStyles.js
@@ -1,29 +1,31 @@
-import signImg from '../../Assets/signImg.jpg'
+import signImg from "../../Assets/signImg.jpg";
 
 export const HomeScreenStyles = {
-    height: "100vh",
-      display: "grid",
-      justifyContent: "center",
-      alignItems: "center", 
+  height: "100vh",
+  display: "grid",
+  justifyContent: "center",
+  alignItems: "center",
 
-    //  backdropFilter: 'blur(8px)',
-    //  WebkitFilter: 'blur(8px)' ,
-      // backgroundImage: `url(${signImg})`,
-      backgroundSize: "cover",
-   
-
-}
+  //  backdropFilter: 'blur(8px)',
+  //  WebkitFilter: 'blur(8px)' ,
+  // backgroundImage: `url(${signImg})`,
+  backgroundSize: "cover",
+};
 
 export const picts = {
   backgroundImage: `url(${signImg})`,
   backgroundSize: "cover",
   backgroundRepeat: "no-repeat",
-  backgroundPosition: "right"
-}
+  backgroundPosition: "right",
+};
 
+export const btn = {
+  backgroundColor: "#424242",
+  mb: 2,
+  mt: 4,
+  ":hover": {
+    bgcolor: "#212121",
+  },
+};
 
-export const btn = {backgroundColor: "#424242",mb:2,mt:4,":hover":{
-  bgcolor:"#212121"
-}};
-
-export const link = { typography: "body1"};
+export const link = { typography: "body1" };
